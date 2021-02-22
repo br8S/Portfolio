0e36af385ac2705a3c8edfaea68777b7fef7b88a
br8S <56180503+br8S@users.noreply.github.com>
2021-02-22 14:59:20 -0800

    added arrow button that will jump to navigation menu

--- navJump.js
+++ navJump.js
@@ -1,3 +1,7 @@
+document.querySelector(".arrow-img").addEventListener('click', function(event) {
+    document.getElementById("jump_to_nav").scrollIntoView({behavior: 'smooth'});
+})
+
 document.querySelector(".home-div").addEventListener('click', function(event) {
     document.getElementById("jump_to_home").scrollIntoView({behavior: 'smooth'});
 })
