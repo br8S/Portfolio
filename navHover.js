7130e2c5a46a1c2899918da1bb96d6ae24ac9eb6
br8S <56180503+br8S@users.noreply.github.com>
2021-02-22 14:47:52 -0800

    added move on hover animation to navigation

--- navHover.js
+++ navHover.js
@@ -1,5 +1,6 @@
 let mouseCursor = document.querySelector('.cursor')
 let socialLinks = document.querySelectorAll('.links')
+let navMove = document.querySelectorAll('.nav-move')
 
 window.addEventListener('mousemove', cursor)
 
@@ -17,3 +18,20 @@ socialLinks.forEach(link => {
             mouseCursor.classList.remove('link-grow')
       })
 })
+
+const animateit = function (e) {
+      const span = this.querySelector('span');
+      const { offsetX: x, offsetY: y } = e,
+      { offsetWidth: width, offsetHeight: height } = this,
+
+      move = 15,
+      xMove = x / width * (move * 1.2) - move,
+      yMove = y / height * (move * 1.2) - move;
+
+      span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+      if (e.type === 'mouseleave') span.style.transform = '';
+};
+
+navMove.forEach(b => b.addEventListener('mousemove', animateit));
+navMove.forEach(b => b.addEventListener('mouseleave', animateit));
