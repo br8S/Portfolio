38d60f997806490d2d22e221a381782d1139c324
br8S <56180503+br8S@users.noreply.github.com>
2021-03-08 20:56:04 -0800

    added a blur effect on hover to projects

---
+++ hoverEffect.js
@@ -0,0 +1,60 @@
+var ballaspt = new hoverEffect({
+    parent: document.querySelector(".ballastpoint-img"),
+    intensity: 0.4,
+    image1: "./images/ballast_point_logo.png",
+    image2: "./images/ballast_point_logo.png",
+    //displacementImage: "./images/halftone.jpg"
+    displacementImage: "./images/heightMap.png",
+    angle: Math.PI/8,
+    imagesRatio: 0.16,
+    speedIn: 1,
+    speedOut: 2
+    
+});
+
+var stussy = new hoverEffect({
+    parent: document.querySelector(".stussy-img"),
+    intensity: 0.4,
+    image1: "./images/stussy.png",
+    image2: "./images/stussy.png",
+    displacementImage: "./images/heightMap.png",
+    angle: Math.PI/8,
+    speedIn: 1,
+    speedOut: 2
+});
+
+var br8s = new hoverEffect({
+    parent: document.querySelector(".br8s-img"),
+    intensity: 0.4,
+    image1: "./images/br8SLOGO.png",
+    image2: "./images/br8SLOGO.png",
+    displacementImage: "./images/heightMap.png",
+    angle: Math.PI/8,
+    imagesRatio: 0.46,
+    speedIn: 1,
+    speedOut: 2
+});
+
+var caltrends = new hoverEffect({
+    parent: document.querySelector(".caltrends-img"),
+    intensity: 0.4,
+    image1: "./images/CALTRENDS.png",
+    image2: "./images/CALTRENDS.png",
+    displacementImage: "./images/heightMap.png",
+    angle: Math.PI/8,
+    imagesRatio: 0.77,
+    speedIn: 1,
+    speedOut: 2
+});
+
+var trackid = new hoverEffect({
+    parent: document.querySelector(".trackid-img"),
+    intensity: 0.4,
+    image1: "./images/TRACKID.png",
+    image2: "./images/TRACKID.png",
+    displacementImage: "./images/heightMap.png",
+    angle: Math.PI/8,
+    imagesRatio: 0.15,
+    speedIn: 1,
+    speedOut: 2
+});
