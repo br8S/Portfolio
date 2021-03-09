7eaadf94c265c69ef9dcc4228f736d3517057404
br8S <56180503+br8S@users.noreply.github.com>
2021-03-09 12:44:14 -0800

    deleted unused hover script

--- workHover.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".project-container").addEventListener("mouseover", function(event){
-    if(event.target.classList.contains("mouse")){
-        // Make the next element sibling visible
-        event.target.nextElementSibling.classList.remove("work-link");
-    }
-});
